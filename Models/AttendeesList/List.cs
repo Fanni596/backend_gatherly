a6832910f19e9596a0d71c1fb49dd6ad5aa8be44
fanni596 <faani596@gmail.com>
2025-06-07 14:45:48 +0500

    invite verification page

--- Models/AttendeesList/List.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GatherlyAPIv0._0._1.Models.AttendeesList
-{
-    public class List
-    {
-            public string Name { get; set; }
-            public string Description { get; set; }
-        
-    }
-}
